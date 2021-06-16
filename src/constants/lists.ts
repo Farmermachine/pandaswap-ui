6905d6b3091701070180b1ad0a1b8d95321a6043
Farmermachine <85457833+Farmermachine@users.noreply.github.com>
2021-06-16 23:25:48 +0200

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,13 @@
-// the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL =
-  'https://raw.githubusercontent.com/Farmermachine/list/main/list.json'
+// used to mark unsupported tokens, these are hosted lists of unsupported tokens
 
-export const DEFAULT_LIST_OF_LISTS: string[] = ['https://raw.githubusercontent.com/Farmermachine/list/main/list.json']
+const PANCAKES_LIST = 'https://gateway.pinata.cloud/ipfs/QmdKy1K5TMzSHncLzUXUJdvKi1tHRmJocDRfmCXxW5mshS'
+
+export const UNSUPPORTED_LIST_URLS: string[] = [PANCAKES_LIST]
+
+// lower index == higher priority for token import
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  PANCAKES_LIST // need to load unsupported tokens as well
+]
+
+// default lists to be 'active' aka searched across
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = []

e1a3afc1931951b452c1d6b82090879f75a8948c
Farmermachine <85457833+Farmermachine@users.noreply.github.com>
2021-06-16 22:53:24 +0200

    Add files via upload

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,4 @@
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
-  'https://raw.githubusercontent.com/baofinance/tokenlists/main/pandaswap-default.json'
-
+  'https://gateway.pinata.cloud/ipfs/QmdKy1K5TMzSHncLzUXUJdvKi1tHRmJocDRfmCXxW5mshS
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]

e895a7de1e5fc0a48b3946729ee88b8752020e70
Farmermachine <85457833+Farmermachine@users.noreply.github.com>
2021-06-17 12:09:57 +0200

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,17 +1,22 @@
 // used to mark unsupported tokens, these are hosted lists of unsupported tokens
-
-const PANCAKES_LIST = 'https://raw.githubusercontent.com/Farmermachine/list/main/list.json'
-
-export const UNSUPPORTED_LIST_URLS: string[] = [PANCAKES_LIST]
-
+//
+//const PANCAKES_LIST = 'https://raw.githubusercontent.com/Farmermachine/list/main/list.json'
+//
+//export const UNSUPPORTED_LIST_URLS: string[] = [PANCAKES_LIST]
+//
 // lower index == higher priority for token import
-export const DEFAULT_TOKEN_LIST_URL: any[] = [
-  PANCAKES_LIST // need to load unsupported tokens as well
-]
-
-export const DEFAULT_LIST_OF_LISTS: string[] = [
-  PANCAKES_LIST // need to load unsupported tokens as well
-]
-
+//export const DEFAULT_TOKEN_LIST_URL: string[] = [
+//  PANCAKES_LIST // need to load unsupported tokens as well
+//]
+//
+//export const DEFAULT_LIST_OF_LISTS: string[] = [
+//  PANCAKES_LIST // need to load unsupported tokens as well
+//]
+//
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = []
+//export const DEFAULT_ACTIVE_LIST_URLS: string[] = []
+
+export const DEFAULT_TOKEN_LIST_URL =
+  'https://raw.githubusercontent.com/Farmermachine/list/main/list.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
